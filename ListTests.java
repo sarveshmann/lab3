fb5ba915a038259eade2d676c148e6311cd63edc
sarveshmann <130100798+sarveshmann@users.noreply.github.com>
2023-05-31 11:08:19 -0700

    Update ListTests.java to do the quiz

--- ListTests.java
+++ ListTests.java
@@ -12,7 +12,18 @@ public class ListTests {
         assertEquals(null, expected1, ListExamples.filter(input1, s -> s.contains("a")));
         
     }
-
+    
+    @Test
+    public void testSubtle() {
+      List<String> s1 = Arrays.asList("a", "b", "a");
+      List<String> s2 = Arrays.asList("a", "b");
+      List<String> result1 = ListExamples.filter(s1, new IsA());
+      List<String> result2 = ListExamples.filter(s2, new IsA());
+      System.out.println(result1.size());
+      System.out.println(result2.size());
+      System.out.println(result1==result2);
+        
+    }
     @Test
     public void testMerge(){
         // inputs
